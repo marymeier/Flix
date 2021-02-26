19f06ae6576bf5b5cec1e0a7d2a7a0cdde4ef291
Mary Meier <mrose041102@gmail.com>
2021-02-26 18:06:31 -0500

    Finished part 2 of flix

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Mary Meier on 2/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
